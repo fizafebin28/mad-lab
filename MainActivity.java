e1d7a8f7fdfb8dfe6de4cf3d78425051597572db
fizafebin28 <fizafebin28@gmail.com>
2026-07-29 14:09:34 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.login;
+
+import static android.app.ProgressDialog.show;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String VALID_USERNAME = "fiza";
+    private static final String VALID_PASSWORD = "password";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText usernameEditText = findViewById(R.id.username);
+        EditText passwordEditText = findViewById(R.id.password);
+        Button loginButton = findViewById(R.id.login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (VALID_USERNAME.equals(username) && VALID_PASSWORD.equals(password)) {
+                    Toast.makeText(MainActivity.this,"Login Successful", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(MainActivity.this,"Invalid Credentials", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+}
